c3ed92a5448699b8e2bcf7bb40a0667729645c35
PotalaSaiVarshitha <96411003+PotalaSaiVarshitha@users.noreply.github.com>
2021-12-21 15:50:17 +0530

    Create TestModel.java

---
+++ core/src/main/java/org/magazine/core/models/TestModel.java
@@ -0,0 +1,9 @@
+package org.magazine.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class)
+public class TestModel {
+
+}
